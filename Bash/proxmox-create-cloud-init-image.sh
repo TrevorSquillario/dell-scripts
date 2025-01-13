d71013ec6b7a53ab9f38a1aac6e481bd433566c0
Trevor Squillario <Trevor_Squillario@Dell.com>
2025-01-13 15:01:20 -0700

    Update proxmox bash script

--- Bash/proxmox-create-cloud-init-image.sh
+++ Bash/proxmox-create-cloud-init-image.sh
@@ -87,7 +87,7 @@ virt-customize --mkdir ${build_info_file_location} --copy-in ${install_dir}build
 virt-customize --copy-in inputrc:/etc -a ${image_path}
 # Add users and ssh keys
 virt-sysprep --root-password "password:/root/secrets/passwd_root" -a ${image_path}
-virt-sysprep --run-command "useradd oseadmin" --run-command "usermod -a -G oseadmin sudo wheel" --password "oseadmin:file:/root/secrets/passwd_oseadmin" -a ${image_path}
+virt-sysprep --run-command "useradd oseadmin" --run-command "usermod -a -G sudo,wheel oseadmin" --password "oseadmin:file:/root/secrets/passwd_oseadmin" -a ${image_path}
 #--ssh-inject "oseadmin:file:/root/secrets/oseadmin.pub" -a ${image_path}
 
 # SSH config
